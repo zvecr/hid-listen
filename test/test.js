62d78d3c5053b2b369b3ee60b1ecac94466dc192
zvecr <git@zvecr.com>
2019-05-23 21:17:52 +0100

    Increase test line coverage

--- test/test.js
+++ test/test.js
@@ -14,6 +14,7 @@ mockery.registerMock('fs', mockFs);
 class DummyHID extends EventEmitter {
 }
 DummyHID.prototype.close = sinon.spy();
+DummyHID.prototype.readTimeout = sinon.stub();
 
 const mockNodeHID = {
   devices: sinon.stub(),
@@ -25,53 +26,114 @@ mockery.enable({
   warnOnUnregistered: false,
 });
 
+sinon.stub(process, 'platform').returns('linux');
+
 const HIDListen = require('../lib/index');
 
-chai.should();
+const should = chai.should();
 
 describe('HIDListen EventEmitter', () => {
-  let mod;
   beforeEach(() => {
-    mod = new HIDListen({ tick: 50, delay: 10 });
+    this.mod = new HIDListen({ tick: 50, delay: 10 });
 
     mockFs.existsSync.returns(true);
     mockFs.readFileSync.returns(Buffer.of());
 
     mockNodeHID.devices.returns([{ usagePage: 0xFF31, usage: 0x0074, path: '/tmp/hidraw1' }]);
+
+    this.originalPlatform = process.platform;
   });
 
   afterEach(() => {
-    mod.removeAllListeners();
+    this.mod.removeAllListeners();
     sinon.restore();
+    DummyHID.prototype.readTimeout.reset();
+
+    Object.defineProperty(process, 'platform', {
+      value: this.originalPlatform,
+    });
   });
 
   it('should not connect when no devices', (done) => {
     mockNodeHID.devices.returns([]);
 
-    mod.on('connect', () => done(new Error('Connect should never be called')));
-    mod.on('tick', () => done());
+    this.mod.on('connect', () => done(new Error('Connect should never be called')));
+    this.mod.on('tick', () => done());
   });
 
   it('should emit connection events', (done) => {
-    mod.on('connect', (device) => {
+    this.mod.on('connect', (device) => {
       mockNodeHID.devices.returns([]);
       device.emit('error');
     });
-    mod.on('disconnect', () => done());
+    this.mod.on('disconnect', () => done());
   });
 
   it('should emit multiple lines on node-hid data', (done) => {
     const lines = [];
 
-    mod.on('connect', (device) => {
+    this.mod.on('connect', (device) => {
       device.emit('data', Buffer.from('line1\nline2\n'));
     });
-    mod.on('data', (data) => {
+    this.mod.on('data', (data) => {
       lines.push(data);
       if (lines.length === 2) {
         lines.should.be.eql(['line1', 'line2']);
         done();
       }
+    });
+  });
+
+  it('should patch in "missing device usage" on linux', (done) => {
+    Object.defineProperty(process, 'platform', {
+      value: 'linux',
+    });
+    mockNodeHID.devices.returns([{ path: '/tmp/hidraw1' }]);
+
+    mockFs.existsSync.returns(true);
+    mockFs.readFileSync.returns(Buffer.of(0x00, 0x31, 0xFF, 0x00, 0x74, 0x00));
+
+    this.mod.on('connect', () => {
+      done();
+    });
+  });
+
+  it('should not patch in "missing device usage" on other platforms', (done) => {
+    Object.defineProperty(process, 'platform', {
+      value: 'other',
+    });
+    mockNodeHID.devices.returns([{ path: '/tmp/hidraw1' }]);
+
+    mockFs.existsSync.throws();
+    mockFs.readFileSync.throws();
+
+    this.mod.on('connect', () => done(new Error('Connect should never be called')));
+    this.mod.on('tick', () => done());
+  });
+
+  it('should patch in support for reads with timeout - success', (done) => {
+    this.mod.on('connect', (device) => {
+      device.readTimeout.returns([]);
+
+      device.read((err, data) => {
+        sinon.assert.calledOnce(device.readTimeout);
+        should.not.exist(err);
+        should.exist(data);
+        done();
+      });
+    });
+  });
+
+  it('should patch in support for reads with timeout - error', (done) => {
+    this.mod.on('connect', (device) => {
+      device.readTimeout.throws([]);
+
+      device.read((err, data) => {
+        sinon.assert.calledOnce(device.readTimeout);
+        should.exist(err);
+        should.not.exist(data);
+        done();
+      });
     });
   });
 });
